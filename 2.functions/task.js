5c34732ebc2a415f96aa5bf1ec090a07169aa814
Viktoria <zheleznova.vicka17@gmail.com>
2026-02-11 20:56:31 +0300

    Решение задания getArrayParams

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,25 @@
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
+
+	let avg = Number((sum / arr.length).toFixed(2));
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
@@ -22,3 +41,4 @@ function averageEvenElementsWorker(...arr) {
 function makeWork (arrOfArr, func) {
 
 }
+/*
